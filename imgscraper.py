6aa27c37b2cea6b7b3c114e1e64d488df4b9ccdd
lordvaider <rishabhvaid1990@gmail.com>
2021-06-21 22:57:31 +0100

    Add some helpful comments to the workflow

--- imgscraper.py
+++ imgscraper.py
@@ -9,18 +9,18 @@ def snipundsave(snx, sny, tlx, tly, brx, bry, imgnum, strt):
     # snip region is (tlx, tly) -> (brx, bry)
     # saved with name snip_<imgnum>
 
-    pag.leftClick(snx, sny)
-    pag.mouseDown(tlx, tly, 'left')
-    pag.mouseUp(brx, bry, 'left')
-    pag.hotkey('ctrl', 's')
-    pag.typewrite('snip_{}'.format(imgnum+strt))
-    pag.hotkey('enter')
+    pag.leftClick(snx, sny)  # Click on new snip button
+    pag.mouseDown(tlx, tly, 'left')  # Start dragging screenshot capture area
+    pag.mouseUp(brx, bry, 'left')  # End dragging screenshot capture area
+    pag.hotkey('ctrl', 's')  # Open Save As window
+    pag.typewrite('snip_{}'.format(imgnum+strt))  # Name the screenshot
+    pag.hotkey('enter')  # Save screenshot
 
 
-pag.leftClick(1705, 13)
+pag.leftClick(1705, 13)  # select the phone screen window
 for img in range(1, 200):
-    pag.press('down')
-    pd.press('enter')
-    snipundsave(1346, 71, 1526, 128, 1917, 984, img, 735)
-    pag.hotkey('alt', 'tab')
-    pag.rightClick(1731, 591)
+    pag.press('down')  # scroll down in the call logs
+    pd.press('enter')  # open up a call log
+    snipundsave(1346, 71, 1526, 128, 1917, 984, img, 0)
+    pag.hotkey('alt', 'tab')  # switch back to phone screen from snip tool
+    pag.rightClick(1731, 591)   # move back to the call logs screen
