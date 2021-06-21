183647f50637958dbe4d7a0cf4779f2d4c10fe0a
lordvaider <rishabhvaid1990@gmail.com>
2021-06-21 22:47:33 +0100

    remove the PAUSE lines

--- imgscraper.py
+++ imgscraper.py
@@ -1,6 +1,7 @@
 import pyautogui as pag
 import pydirectinput as pd
 
+pag.PAUSE = 0.3
 
 def snipundsave(snx, sny, tlx, tly, brx, bry, imgnum, strt):
 
@@ -9,26 +10,17 @@ def snipundsave(snx, sny, tlx, tly, brx, bry, imgnum, strt):
     # saved with name snip_<imgnum>
 
     pag.leftClick(snx, sny)
-    pag.PAUSE = 0.3
     pag.mouseDown(tlx, tly, 'left')
-    pag.PAUSE = 0.3
     pag.mouseUp(brx, bry, 'left')
-    pag.PAUSE = 0.3
     pag.hotkey('ctrl', 's')
-    pag.PAUSE = 0.3
     pag.typewrite('snip_{}'.format(imgnum+strt))
-    pag.PAUSE = 0.3
     pag.hotkey('enter')
-    pag.PAUSE = 0.3
 
 
 pag.leftClick(1705, 13)
 for img in range(1, 200):
     pag.press('down')
-    pag.PAUSE = 0.3
     pd.press('enter')
-    pag.PAUSE = 0.3
     snipundsave(1346, 71, 1526, 128, 1917, 984, img, 735)
-    pag.PAUSE = 0.3
     pag.hotkey('alt', 'tab')
     pag.rightClick(1731, 591)
